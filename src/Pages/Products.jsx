6227d2b16b927ba1f8b0ed63e9ad4678c1a94dd8
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-07-03 23:40:33 +0530

    cleaned file

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,24 +1,14 @@
 import { React } from "react";
 import './assets/css/product.css';
 import { ProductCard, Nav } from '../Components';
-// import LoadingScreen from "react-loading-screen";
 import { useProducts } from "../Contexts";
 
 export function Products() {
 
   const { productsState, productsDispatch } = useProducts();
-  // const [loader, setLoader] = useState(false)
 
   return (
     <>
-    {/* <LoadingScreen
-          loading={loader}
-          bgColor="#f1f1f1"
-          spinnerColor="#9ee5f8"
-          textColor="#676767"
-          text="Loading... Please Wait!"
-    >
-    </LoadingScreen> */}
     <div className="filters">
       <div>
         <p>Sort By Price</p>
